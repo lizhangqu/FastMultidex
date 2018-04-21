a16cdaf603470ccbd38379817609b765378c22d5
李樟取 <lizhangqu@weidian.com>
2018-04-21 22:59:09 +0800

    * plugin

---
+++ buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexPlugin.groovy
@@ -0,0 +1,13 @@
+package io.github.lizhangqu.fastmultidex
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FastMultidexPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+}
