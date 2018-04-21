f06dfb1246ccd3f7eb3215fddf37c4425b4869e5
李樟取 <lizhangqu@weidian.com>
2018-04-21 22:59:35 +0800

    * gradle plugin version method

--- buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexPlugin.groovy
+++ buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexPlugin.groovy
@@ -5,6 +5,20 @@ import org.gradle.api.Project
 
 class FastMultidexPlugin implements Plugin<Project> {
 
+    @SuppressWarnings("GrMethodMayBeStatic")
+    String getAndroidGradlePluginVersionCompat() {
+        String version = null
+        try {
+            Class versionModel = Class.forName("com.android.builder.model.Version")
+            def versionFiled = versionModel.getDeclaredField("ANDROID_GRADLE_PLUGIN_VERSION")
+            versionFiled.setAccessible(true)
+            version = versionFiled.get(null)
+        } catch (Exception e) {
+            version = "unknown"
+        }
+        return version
+    }
+    
     @Override
     void apply(Project project) {
 
