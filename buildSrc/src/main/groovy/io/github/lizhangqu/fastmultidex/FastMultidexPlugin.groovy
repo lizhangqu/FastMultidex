2ed8c0a474de008e80f4e9a2ac1f4a63cabde49a
李樟取 <lizhangqu@weidian.com>
2018-04-21 23:01:59 +0800

    * find TransformTask by type

--- buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexPlugin.groovy
+++ buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexPlugin.groovy
@@ -1,12 +1,14 @@
 package io.github.lizhangqu.fastmultidex
 
+import com.android.build.gradle.internal.pipeline.TransformTask
+import com.android.builder.core.VariantConfiguration
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskCollection
 
 class FastMultidexPlugin implements Plugin<Project> {
 
-    @SuppressWarnings("GrMethodMayBeStatic")
-    String getAndroidGradlePluginVersionCompat() {
+    static String getAndroidGradlePluginVersionCompat() {
         String version = null
         try {
             Class versionModel = Class.forName("com.android.builder.model.Version")
@@ -18,7 +20,22 @@ class FastMultidexPlugin implements Plugin<Project> {
         }
         return version
     }
-    
+
+    static List<TransformTask> findTransformTaskByTransformType(Project project, VariantConfiguration variantConfiguration, Class<?> transformClass) {
+        List<TransformTask> transformTasksList = new ArrayList<>()
+        TaskCollection<TransformTask> transformTasks = project.getTasks().withType(TransformTask.class)
+        SortedMap<String, TransformTask> transformTaskSortedMap = transformTasks.getAsMap()
+        String variantName = variantConfiguration.getFullName()
+        transformTaskSortedMap.each { String taskName, TransformTask transformTask ->
+            if (variantName == transformTask.getVariantName()) {
+                if (transformTask.getTransform().getClass() == transformClass) {
+                    transformTasksList.add(transformTask)
+                }
+            }
+        }
+        return transformTasksList
+    }
+
     @Override
     void apply(Project project) {
 
