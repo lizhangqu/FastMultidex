d7ab3ec92532dd20064edb644d9182453178cec4
李樟取 <lizhangqu@weidian.com>
2018-04-22 13:25:10 +0800

    * app

---
+++ app/src/main/java/io/github/lizhangqu/app/App.java
@@ -0,0 +1,16 @@
+package io.github.lizhangqu.app;
+
+import android.app.Application;
+
+/**
+ * @author lizhangqu
+ * @version V1.0
+ * @since 2018-04-22 13:24
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
