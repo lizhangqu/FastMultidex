2bc7e69d5bdcb6cd6390cde2f9d75b95b3c6da68
李樟取 <lizhangqu@weidian.com>
2018-04-23 18:44:40 +0800

    * add demo

--- app/src/main/java/io/github/lizhangqu/app/App.java
+++ app/src/main/java/io/github/lizhangqu/app/App.java
@@ -1,6 +1,9 @@
 package io.github.lizhangqu.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.util.Log;
 
 import com.alibaba.fastjson.JSON;
 
@@ -10,12 +13,22 @@ import com.alibaba.fastjson.JSON;
  * @since 2018-04-22 13:24
  */
 public class App extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        MultiDex.install(base);
+        super.attachBaseContext(base);
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         try {
-            JSON.parse("");
+            Object parse = JSON.parse("{\n" +
+                    "  \"a\":\"a\",\n" +
+                    "  \"b\":1,\n" +
+                    "  \"c\":true   \n" +
+                    "}");
+            Log.e("TAG", "parse901:" + parse);
         } catch (Exception e) {
             e.printStackTrace();
         }
