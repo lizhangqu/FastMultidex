e7e9ba8b83cd321b6a034838ddc0c26cbbdd2611
lizhangqu <lizhangqu@weidian.com>
2018-04-27 12:24:13 +0800

    * reduce

--- buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexExtension.groovy
+++ buildSrc/src/main/groovy/io/github/lizhangqu/fastmultidex/FastMultidexExtension.groovy
@@ -4,7 +4,7 @@ package io.github.lizhangqu.fastmultidex
 class FastMultidexExtension {
     int mainDexMaxNumber = 1000
     int jarMergeMaxNumber = 300
-    int maxMethodNumber = 65535
-    int maxFieldNumber = 65535
+    int maxMethodNumber = 50000
+    int maxFieldNumber = 50000
     boolean dexMerge = true
 }
